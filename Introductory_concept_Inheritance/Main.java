66cc3a35ecc8ce0774a21a56a845e33e3f1783c9
Abel Woldemichael <abelllselam@gmail.com>
2025-04-15 12:55:26 -0400

    Inheritance practice and question.

---
+++ Introductory_concept_Inheritance/Main.java
@@ -0,0 +1,30 @@
+class Vehicle {
+    public String brand;
+
+    public void startEngine(){
+        System.out.println("Engine started for Vehicle");
+    }
+}
+
+class Car extends Vehicle {
+    public int numberOfDoors;
+
+     public void displayInfo(){
+        System.out.println("Brand: " + this.brand);
+        System.out.println("Number of doors: " + this.numberOfDoors);
+     }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car auto = new Car();
+
+        auto.brand = "KIA";
+        auto.numberOfDoors = 4;
+
+
+        auto.startEngine();
+        auto.displayInfo();
+        
+    }
+}
---
+++ Introductory_concept_Inheritance/inheritancequestion.md
@@ -0,0 +1,16 @@
+Practice Problem – Inheritance
+Create a simple Java program that models Vehicles.
+
+Create a base class called Vehicle with:
+
+A brand field
+
+A method called startEngine() that prints: "Engine started for Vehicle"
+
+Create a subclass called Car that:
+
+Inherits from Vehicle
+
+Has an additional field called numberOfDoors
+
+Has a method called displayInfo() that prints out the brand and number of doors.
