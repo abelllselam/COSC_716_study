69d52141a4094c6152a98be491721b589c3c8f43
Abel Woldemichael <abelllselam@gmail.com>
2025-04-15 12:27:20 -0400

    Encapsulation exercise and the question is in the readme file

---
+++ Introductory_concept/Main.java
@@ -0,0 +1,78 @@
+package Introductory_concept;
+
+class Book {
+    private String title;
+    private String author;
+    private double price;
+    private int publicationYear;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setPrice(double price){
+        if (price < 0){
+            System.out.println ("Price can not be negative.");
+            return;
+        }
+
+        this.price = price;
+        
+    }
+
+    public void setPublicationYear(int publicationYear){
+        this.publicationYear = publicationYear;
+    }
+
+}
+
+class Library {
+    Book b = new Book();
+
+    public void addBook() {
+        b.setAuthor("Abel"); 
+        b.setTitle("My book");
+        b.setPrice(10);
+        b.setPublicationYear(2025);
+        
+    }
+
+    public void display(){
+        System.out.println("Author: " + b.getAuthor());
+        System.out.println("The name of the book: " + b.getTitle());
+        System.out.println("Price $" + b.getPrice());
+        System.out.println("Publication Year " + b.getPublicationYear());
+    }
+}
+
+
+public class Main {
+    public static void main(String [] args){
+        Library lib  = new Library();
+            lib.addBook();
+            lib.display();
+    }
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 COSC 716 study materials
+
+💡 Encapsulation Practice Problem
+Problem Statement:
+You are tasked with designing a Library Management System where each Book has:
+
+A title
+
+An author
+
+A price
+
+A publication year
+
+Implement encapsulation in the Book class. The class should:
+
+Hide its fields (title, author, price, and publicationYear) using private access.
+
+Provide getter and setter methods for each field.
+
+Ensure that the price cannot be set to a negative value.
+
+You also need to create a Library class that manages a collection of books. The Library should:
+
+Be able to add books.
+
+Display the details of all books.
+
+📄 Class Design Outline:
+Book Class
+
+Fields: title, author, price, publicationYear
+
+Methods:
+
+Getters and setters for each field.
+
+Custom setter to ensure price cannot be negative.
+
+Library Class
+
+Field: A List of books.
+
+Methods:
+
+addBook(Book book) to add a book.
+
+displayBooks() to print details of all books.
